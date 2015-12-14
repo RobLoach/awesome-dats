799b8677f9d4e511f0fa5064016191b81802a984
Rob Loach <robloach@gmail.com>
2015-12-14 18:12:22 -0500

    Add build process

--- README.md
+++ README.md
@@ -1,10 +1,27 @@
 # Awesome DATs
 
-A curated list of awesome dat files.
+A curated list of awesome DAT files.
 
 ## Usage
 
 1. Open [clrmamepro](http://mamedev.emulab.it/clrmamepro/)
-2. Switch to the www profiler
-3. Click Add Site...
-4. Use `http://robloach.github.io/awesome-dats/dats.xml` for the URL, "Awesome-DATs" for the name
+2. Switch to the [WWW Profiler](http://mamedev.emulab.it/clrmamepro/docs/wwwprofiler.txt) mode
+3. Click *Add Site...*
+4. For the site alias, use *Awesome-DATs*
+5. For the URL, use [`http://robloach.github.io/awesome-dats/dats.xml`](http://robloach.github.io/awesome-dats/dats.xml)
+
+## Development
+
+1. Make sure [Node.js](https://nodejs.org/en/) `>= 4.2.0` is installed
+  ```
+  node --version
+  ```
+2. Add DAT files to a subfolder
+3. Run `npm install` to install dependencies
+4. Build with `npm test`
+5. Submit pull requests with your changes
+
+## License
+
+- [MIT](LICENSE.md)
+- [No-Intro Disclaimer](http://www.no-intro.org/disclaimer.htm)
---
+++ build.js
@@ -0,0 +1,62 @@
+var glob = require('glob');
+var fs = require('fs');
+var path = require('path');
+
+var regexVersion = /version\s*#*>*[\d+\-*+?x?\d*]+/
+
+function sortDat(a,b) {
+  if (a.path < b.path)
+    return -1;
+  if (a.path > b.path)
+    return 1;
+  return 0;
+}
+
+glob('**/*.dat', function (err, files) {
+  if (err) {
+    throw new Error(err);
+  }
+  else {
+    var dats = []
+
+    // Iterate through each file.
+    for (var i in files) {
+      if (files.hasOwnProperty(i)) {
+        // Get the contents.
+        var file = files[i];
+        var contents = fs.readFileSync(file, 'utf8');
+
+        // Find the DAT information.
+        console.log('Processing: ' + files[i])
+        var match = regexVersion.exec(contents);
+        var version = match[0];
+        version = version.replace('version', '').replace(' ', '').replace('>', '').replace('#', '');
+        var name = path.basename(files[i], '.dat');
+        var author = path.dirname(files[i])
+        console.log('    ' + author + ': ' + name + ' (' + version + ')')
+
+        // Append the information to the output.
+        dats.push({
+          path: files[i],
+          name: name,
+          version: version,
+          author: author
+        });
+      }
+    }
+
+    var output = '<clrmamepro>\n';
+    dats.sort(sortDat);
+    for (var x in dats) {
+      output = output + '  <datfile>\n' +
+        '    <name>' + dats[x].name + '</name>\n' +
+        '    <description>' + dats[x].name + '</description>\n' +
+        '    <version>' + dats[x].version + '</version>\n' +
+        '    <author>' + dats[x].author + '</author>\n' +
+        '    <url>https://robloach.github.io/awesome-dats/' + dats[x].path + '</url>\n' +
+        '  </datfile>\n';
+    }
+    output = output + '</clrmamepro>\n';
+    fs.writeFileSync('dats.xml', output);
+  }
+})
